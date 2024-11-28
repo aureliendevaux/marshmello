7370bc0a1106cfd8208787b5e64f14ae2804e70c
Aurélien Devaux <oss@aureldvx.net>
2024-11-28 15:54:08 +0100

    test(api): configure auth with api client

--- packages/api/tests/bootstrap.ts
+++ packages/api/tests/bootstrap.ts
@@ -1,12 +1,20 @@
 import type { Config } from '@japa/runner/types';
 
+import { authApiClient } from '@adonisjs/auth/plugins/api_client';
 import app from '@adonisjs/core/services/app';
 import testUtils from '@adonisjs/core/services/test_utils';
+import { sessionApiClient } from '@adonisjs/session/plugins/api_client';
 import { apiClient } from '@japa/api-client';
 import { assert } from '@japa/assert';
 import { pluginAdonisJS } from '@japa/plugin-adonisjs';
 
-export const plugins: Config['plugins'] = [assert(), apiClient(), pluginAdonisJS(app)];
+export const plugins: Config['plugins'] = [
+	assert(),
+	apiClient(),
+	pluginAdonisJS(app),
+	sessionApiClient(app),
+	authApiClient(app),
+];
 
 export const runnerHooks: Required<Pick<Config, 'setup' | 'teardown'>> = {
 	setup: [() => testUtils.db().migrate()],
